6eca6cd5ed44d05c9dd9c4382f4a738dca9195b1
blackbrokkoli <kastomat@gmail.com>
2023-08-31 23:09:18 +0300

    display random field and four answers

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,10 @@ import { ref } from "vue";
 
 let randomNumber = ref([]);
 let numberBank = [];
+const fieldUsedAsPrompt = ref(0);
+const fieldUsedAsAnswer = ref(1);
+const possibleAnswers = ref([]);
+const prompt = ref("")
 let unitsPracticedToday = 0;
 let unitsPracticedYesterday = 0;
 // see if numberBankis in localStorage, if so, load it,  if not, set it to the imported numbers
@@ -25,17 +29,41 @@ if (!localStorage.getItem("stats")) {
 }
 
 function getRandomNumber() {
-  isRevealed.value = false;
+  guessMade.value = false;
   console.log("picking number from", numberBank);
   const newNumber =
     numberBank.value[Math.floor(Math.random() * numberBank.value.length)];
+
+  // pick a nr between 0 and 3
+  fieldUsedAsPrompt.value = Math.floor(Math.random() * 4);
+  prompt.value = newNumber[fieldUsedAsPrompt.value];
+  // pick a nr between 0 and 3 that is not the same as fieldUsedAsPrompt
+  fieldUsedAsAnswer.value = Math.floor(Math.random() * 4);
+  while (fieldUsedAsAnswer.value === fieldUsedAsPrompt.value) {
+    fieldUsedAsAnswer.value = Math.floor(Math.random() * 4);
+  }
+  // have a 4 possible answer fields: one is the correct one, the rest is wrong ones picked from the data
+  possibleAnswers.value = [newNumber[fieldUsedAsAnswer.value]];
+  for (let i = 0; i < 3; i++) {
+    let newWrongAnswer =
+      numberBank.value[Math.floor(Math.random() * numberBank.value.length)][
+        fieldUsedAsAnswer.value
+      ];
+    while (possibleAnswers.value.includes(newWrongAnswer)) {
+      newWrongAnswer =
+        numberBank.value[Math.floor(Math.random() * numberBank.value.length)][
+          fieldUsedAsAnswer.value
+        ];
+    }
+    possibleAnswers.value.push(newWrongAnswer);
+  }
 
   randomNumber.value = newNumber;
   // save the numberBankto localStorage (doesn't make that much sense here in the code but whatever)
   localStorage.setItem("numberBank", JSON.stringify(numberBank.value));
 }
 
-let isRevealed = ref(false);
+let guessMade = ref(false);
 
 let valueEase = ref(null);
 let valueCorrect = ref(null);
@@ -49,10 +77,23 @@ getRandomNumber();
   <div
     class="card bg-gray-600 shadow-xl m-4 p-4 flex flex-col items-center w-full max-w-screen-xl"
   >
-    <div class="card-body text-xl">
-      {{ randomNumber}}
+    <div class="card-body">
+      <div id="prompt"  class="text-2xl">
+        {{ prompt }}
+      </div>
     </div>
-    <div class="card-actions justify-end mt-6 pt-2">
+
+    <div class="card-actions flex-col justify-end mt-6 pt-2">
+      <button
+        class="btn text-xl w-full max-w-1/3"
+        v-for="answer in possibleAnswers"
+        @click="handleAnswer(answer)"
+      >
+        {{ answer }}
+      </button>
+    </div>
+
+    <div class="card-actions justify-end mt-6 pt-2 hidden">
       <button class="btn btn-primary" @click="getRandomNumber">Next</button>
     </div>
   </div>
