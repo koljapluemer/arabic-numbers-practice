e0bb13c3046e5414c6cff2e1cdb8b0bde2f5ae14
blackbrokkoli <kastomat@gmail.com>
2023-09-28 19:23:18 +0300

    add share buttons

--- src/App.vue
+++ src/App.vue
@@ -474,6 +474,38 @@ function convertNumberToArabicScript(number) {
     </div>
   </main>
   <footer class="border-t-2 mt-10 w-full p-4 text-sm">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"
+    />
+    <div id="socials" class="mb-4 flex justify-center gap-2">
+      <!-- Twitter Share Button -->
+      <a
+        href="https://twitter.com/share?url=https://arabic-numbers.koljapluemer.com"
+        target="_blank"
+        rel="noopener"
+      >
+        <i class="fab fa-twitter share-icon"></i> Share on Twitter
+      </a>
+
+      <!-- Reddit Share Button -->
+      <a
+        href="https://www.reddit.com/submit?url=https://arabic-numbers.koljapluemer.com"
+        target="_blank"
+        rel="noopener"
+      >
+        <i class="fab fa-reddit share-icon"></i> Share on Reddit
+      </a>
+
+      <!-- Hacker News Share Button -->
+      <a
+        href="https://news.ycombinator.com/submitlink?u=https://arabic-numbers.koljapluemer.com&amp;t=Arabic%20Numbers%20Converter"
+        target="_blank"
+        rel="noopener"
+      >
+        <i class="fab fa-hacker-news share-icon"></i> Share on Hacker News
+      </a>
+    </div>
     <ul class="flex flex-col gap-2">
       <li>
         Transliterations are Egyptian Arabic. For now. I'm working on an option
